b4f5b53b7c3d4564a2f3cf12ec34e35367fca82d
karye <karye.webb@gmail.com>
2020-01-08 15:29:32 +0100

    Ny övning

---
+++ javascript/6-labbar/spel/spel.js
@@ -0,0 +1,26 @@
+/* 
+1973
+Bankrånare Janne Olssons gisslan uttrycker stor sympati för sin förövare och det interbationellt gångbara begreppet "stockholmssyndrom" föds.
+1953
+Stokholm firar 700 år.
+1987
+Västtyske 19-åringen Matthias Rust taxar in ett hyrt Cessna-plan på Röda Torget. KBG lägger genast vantarna på honom.
+1975
+Spanske diktatorn Francisco Franco dör.
+1978
+Sekten Folkets Tempel genomför ett massjälvmord, 913 medlemmar dör.
+1960
+Hitchcocks film Psycho har premiär.
+1945
+Efter att ha blivit refuserad av Bonniers ges en omarbetad version av Pippi Långstrump ut av Rabén och Sjögren.
+1841
+Edgare Allan Poes Morden på Rue Morgue, en föregångare till detektivberättelsen, finns nu att läsa.
+1963
+AB Svensk Bilprovning bildas.
+1477
+Nordens första universitet grundas i Uppsala.
+1925
+Radiotjänst startar sina sändningar.
+1967
+Sexdagarskriget mellan Israel och arabiska grannstater bryter ut.
+*/
