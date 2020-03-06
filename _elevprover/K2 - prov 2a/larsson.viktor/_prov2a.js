16eebb9c06cc71a9bb2e6af1bf28586d6f468279
karye <karye.webb@gmail.com>
2020-03-06 12:47:34 +0100

    Städar

---
+++ _elevprover/K2 - prov 2a/larsson.viktor/_prov2a.js
@@ -0,0 +1,69 @@
+/* Värden som du behöver */
+var elementLabel1 = document.querySelector("#vara");
+var elementLabel2 = document.querySelector("#pris");
+var elementKnapp = document.querySelector("button");
+var elementInköp = document.querySelector("#små");
+var elementInköp = document.querySelector("#stora");
+var elementSumma = document.querySelector(".total total1");
+var elementSumma2 = document.querySelector(".total total2");
+var elementH5 = document.querySelector("h5");
+
+/* Visa inköpslistan */
+
+
+/* Användaren klickar på "lägga till" */
+elementKnapp.addEventListener("input", stoppaInVärden);
+elementLabel2.addEventListener("input", stoppaInPris);
+
+/* Läs av rutorna vara och pris */
+function stoppaInVärden(){
+var elementLabel1 = ("");
+}
+function
+
+/* Om pris är mindre än 100 */
+
+if (stoppaInVärden == <100) {
+  var 
+    
+} else {
+
+}
+
+/* Skriv ut vara och pris i listan "små inköp" */
+addEventListener
+
+/* Räkna ut summa i listan "små inköp" */
+
+
+/* Visa nya summan "små inköp" */
+
+
+/* Skriv ut vara och pris i listan "stora inköp" */
+
+
+/* Räkna ut summa i listan "stora inköp" */
+
+
+/* Visa nya summan "stora inköp" */
+
+
+/* Om totala summan 1000 */
+if (condition) {
+    style.color = "red";
+}
+
+/* Slut */
+elementKnapp.addEventListener("click", räknaUt);
+
+function räknaUt(){
+var värde = value ;
+}
+
+var elementLabel1 = addEventListener("input", vara);
+
+function vara(){
+    
+}
+
+
